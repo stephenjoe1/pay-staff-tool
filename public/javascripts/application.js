dad21d27eb9ed7a0d932bd1799f3757cce913cf7
stephen mccarthy <stephen.mccarthy@digital.cabinet-office.gov.uk>
2015-04-28 18:10:48 +0100

    updates made to flow pages

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,3 +4,23 @@ $(function(){
         window.location = $(this).find('input[type="radio"]:checked').val();
     });
 });
+
+$(document).ready(function() {
+	var stickyNavTop = $('.purchase-details-small').offset().top;
+	 
+	var stickyNav = function(){
+	var scrollTop = $(window).scrollTop();
+	      
+	if (scrollTop > stickyNavTop) { 
+	    $('.purchase-details-small').addClass('sticky');
+	} else {
+	    $('.purchase-details-small').removeClass('sticky'); 
+	}
+	};
+	 
+	stickyNav();
+	 
+	$(window).scroll(function() {
+	    stickyNav();
+	});
+});
