c4e00daab05ac6cc0b9965fd5a07539b3281bd2e
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2015-05-07 16:23:29 +0100

    Clear the session on accessing the index page too.

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,21 @@
+function clear_session(req) {
+  console.log("Clearing the session.")
+  delete req.session
+}
+
 module.exports = {
   bind : function (app) {
 
     app.get('/', function (req, res) {
-      console.log("Clearing the session.")
-      delete req.session
+      clear_session(req);
       res.render('index');
     });
+
+    app.get('/index', function (req, res) {
+      clear_session(req);
+      res.render('index');
+    });
+
 
     app.get('/examples/template-data', function (req, res) {
       res.render('examples/template-data', { 'name' : 'Foo' });
