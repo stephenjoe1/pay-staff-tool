e66b2c2ae43233e29755ebb4cea38154c43d8ae4
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2015-10-02 14:32:59 +0100

    Validation on debit card pages.
    This expects the following test data to be submitted on the debit card
    pages, and will fail with govuk elements style error styling if it's
    missing:
    * Name: present
    * Card number: 4111123456781111
    * Expiry date: 03/18
    * Security Number: 123
    * First line of address: present
    * Town or city: present
    * Postcode: present

--- app/routes.js
+++ app/routes.js
@@ -54,6 +54,78 @@ module.exports = {
       });
     });
 
+    function trimmed(str) {
+      return (str || "").trim();
+    }
+
+    function squashed(str) {
+      return (str || "").replace(/\s/g, "");
+    }
+
+
+    function validateCardDetails(req, res, errorView) {
+      var errors = {};
+
+      if (squashed(req.body['card-number']) !== "4111123456781111") {
+        errors["error-card-number"] = true;
+      }
+
+      if (trimmed(req.body['full-name']) === "") {
+        errors["error-full-name"] = true;
+      }
+      if (squashed(req.body['exp-month']) !== "03" && squashed(req.body['exp-month']) !== "3") {
+        errors["error-expiry-date"] = true;
+      }
+      if (squashed(req.body['exp-year']) !== "18") {
+        errors["error-expiry-date"] = true;
+      }
+      if (squashed(req.body['security-number']) !== "123") {
+        errors["error-csc"] = true;
+      }
+      if (trimmed(req.body['building-and-street-1']) === "") {
+        errors["error-billing-address"] = true;
+      }
+      if (trimmed(req.body['town-or-city']) === "") {
+        errors["error-billing-address"] = true;
+      }
+      if (trimmed(req.body['postcode']) === "") {
+        errors["error-billing-address"] = true;
+      }
+      console.log(errors);
+      if (Object.keys(errors).length > 0) {
+        res.render(errorView, merge(true, req.body, errors));
+        return false;
+      }
+      return true ;
+    }
+
+    app.post('/borchester-permit-debit-card-payment', function(req, res) {
+      if (!validateCardDetails(req, res, 'borchester-permit-debit-card-payment')) {
+        return;
+      }
+      res.redirect(req.originalUrl);
+    });
+
+    app.post('/borchester-penalty-debit-card-payment-review', function(req, res) {
+      if (!validateCardDetails(req, res, 'borchester-penalty-debit-card-payment')) {
+        return;
+      }
+      res.redirect(req.originalUrl);
+    });
+
+    app.post('/borchester-council-tax-debit-card-payment-review', function(req, res) {
+      if (!validateCardDetails(req, res, 'borchester-council-tax-debit-card-payment')) {
+        return;
+      }
+      res.redirect(req.originalUrl);
+    });
+
+    app.post('/borchester-penalty-debit-card-provider-down', function(req, res) {
+      if (!validateCardDetails(req, res, 'borchester-penalty-debit-card-failing-payment')) {
+        return;
+      }
+      res.redirect(req.originalUrl);
+    });
 
     app.get('/examples/template-data', function (req, res) {
       res.render('examples/template-data', { 'name' : 'Foo' });
